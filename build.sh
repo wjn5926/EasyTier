15e110b38b1f016a653cc055ce3f9e8f00ea172a
wjn5926 <81164375+wjn5926@users.noreply.github.com>
2025-09-21 13:46:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-apt-get update && apt-get install -y clang libclang-dev llvm-dev build-essential pkg-config cmake
+set -eux
+
+# 安装构建依赖，包括 protoc
+apt-get update
+apt-get install -y protobuf-compiler clang libclang-dev llvm-dev build-essential pkg-config cmake
+
+# 设置环境变量
 export LIBCLANG_PATH=/usr/lib/llvm-14/lib
 export CC=clang
+
+# 编译项目
 cargo build --release --bin easytier-core
